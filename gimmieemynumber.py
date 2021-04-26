6c75465eb929705226fd5067d428d01f1f6183a8
Owner <owner@kevin-mbp.home>
2021-04-26 16:23:20 -0400

    Edit these labs and done

---
+++ gimmieemynumber.py
@@ -0,0 +1,7 @@
+string = input("Gimme a number greter than 100...")
+num = int(string)
+while num < 100:
+    print(str(num) + "is less than 100, Try again , Ive got all day")
+    string = prompt(usernum + "is less than 100,Try again Ive got all day...")
+    num = int(string)
+print(str(num) + "is greater than 100! Finally! Great Job!")
